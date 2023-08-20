84e2f4ace82df24d0d7e6d29656203abe7d2ef81
Allen Yu <allen.yu@wayne.edu>
2023-08-20 19:20:10 -0400

    Task 1 Completed + Unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,18 +9,34 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    """If this assert is actually checking whether the 'Bid price' is greater than 'Ask price'..."""
+    #for quote in quotes:
+    #  self.assertGreater(quote['top_bid']['price'], quote['top_ask']['price'], "Bid not greater than ask")
 
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_checkPriceAIsZero(self):
+    price_a = 0
+    price_b = 123.91
+    """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getRatio(price_a, price_b), 0)
 
-
+  def test_getRatio_checkPriceBIsZero(self):
+    price_a = 123.91
+    price_b = 0
+    """ ------------ Add the assertion below ------------ """
+    with self.assertRaises(ZeroDivisionError):
+      x = getRatio(price_a/price_b)
 
 if __name__ == '__main__':
     unittest.main()
